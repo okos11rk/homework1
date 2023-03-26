53f5a1c516e7c7496ac39f6f53dc4cafb0825aa6
okos11rk <128313033+okos11rk@users.noreply.github.com>
2023-03-26 13:29:54 +0300

    Add files via upload

---
+++ дз 2.py
@@ -0,0 +1,21 @@
+print("задание 1")
+a = 5
+b = 10
+s = a* b
+p = a + b
+print(s)
+print(p)
+print()
+print("задание 2")
+x = 32345
+a= 4
+b = 5
+i = 3
+o = 2
+c = a**b
+d = c * 3
+
+h = d / (o - i) 
+print(c)
+print(d)
+print(h)
